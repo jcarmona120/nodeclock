09f9c4fac26327a0a89540c02ceee87efed3bd56
Jawann Carmona <carmona.jawann@gmail.com>
2017-05-31 22:42:22 -0400

    Served static HTML file.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const moment = require('moment');
+const path = require('path') //To get the file path to a folder we will require the native Node moduel, path.
 
+app.use(express.static(path.join(__dirname + '/public'))); //dirname contains file path of current folder. second parameter is folder containing static resources.
 
 app.get('/', function (req, res) {
   var day = moment().format("ddd, hA");
